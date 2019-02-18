6442e68a3cab8fbb5d6b52eccaae6c7083a8e70e
Clement Roussillon <clement.playstation@gmail.com>
2019-02-18 10:17:49 +0100

    Add currentNavItem in red when is active + change sidenav width to be coherent with header

--- public/javascripts/views/menu.js
+++ public/javascripts/views/menu.js
@@ -4,7 +4,14 @@
 currentUser = 'none';
 $(function () {
     var btn = document.querySelector('.nav__button'), container = document.querySelector('.conteneur');
-
+    $('.bold').map(function(key, navItem) {
+        if($(navItem)[0].children[0].attributes.hasOwnProperty('href')){
+            $($(navItem)[0].children[0]).css('color', 'rgba(0,0,0,0.87)');
+            if(document.URL.indexOf($(navItem)[0].children[0].attributes.href.value) !== -1 && $(navItem)[0].children[0].attributes.href.value !=='/'){
+                $($(navItem)[0].children[0]).css('color', '#B71C1C');
+            }
+        }
+    });
     function is_touch_device() {
         try {
             document.createEvent("TouchEvent");
